06a1555dcdadea4c309b48703fc38592b4313527
saad <gosaadmakhal@gmail.com>
2021-04-05 03:41:45 +0200

    first commit ya saad

---
+++ app/src/main/java/com/example/liveala/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.liveala;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Home.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
